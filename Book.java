d39a01e3cbced94c1104027b5fbf6306c76c11b6
Hanzhang Jiang <hj398@cornell.edu>
2022-07-27 23:54:47 -0400

    solve conflict

--- Book.java
+++ Book.java
@@ -2,11 +2,13 @@
 public class Book {
     String title;
     String author;
+    int year;
 
     /** Create a new book with title b. */
-    public Book(String t, String a) {
+    public Book(String t, String a, int y) {
         title= t;
         author= a;
+        year= y;
     }
 
     /** Print this book. */
@@ -16,7 +18,7 @@ public class Book {
 
     /** Some book examples. */
     public static void main(String[] args) {
-        Book b= new Book("A Clockwork Orange", "author");
+        Book b= new Book("A Clockwork Orange", "author", 0000);
         b.printBook();
     }
 }
